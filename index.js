5ba1685f27fbe9295eba0517665aaf6d9a6028c5
georgio893479874 <129616431+georgio893479874@users.noreply.github.com>
2023-07-27 21:17:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
-    res.send('Hello45');
+    res.send('Hello');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.json({
+      success: true,
+    });
 });
 
 app.listen(port, (err) => {
     if(err) {
         return console.log(err);
     }
-
     console.log('not error');
 });
-// http://localhost:3000/
+
