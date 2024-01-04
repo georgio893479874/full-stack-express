770b5816b2ebd5cad7e4090bf67dd53d705c46e0
georgio893479874 <129616431+georgio893479874@users.noreply.github.com>
2024-01-04 21:50:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator';
 
-mongoose.connect("");
+mongoose.connect("").then(() => console.log('Data base running')).catch((err) => console.log('Data base running', err));
 
 let productsSchema = new Schema({
         "id": Number,
@@ -20,6 +20,9 @@ let productsSchema = new Schema({
         "images": Array,
 })
 
+let Products = mongoose.model('product', productsSchema);
+let products = await Products.find({});
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,18 +31,45 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get('/shop', async (req, res) => {
-    products = await Products.find({});
-    res.render('template-shop', {products});
-})
-
 app.get('/products/:number', (req, res) => {
     let number = +req.params.number;
     res.render('template-goods.ejs', {data: products[number+1]});
 })
 
+app.post('/new-product', async (req, res) => {
+    let name = req.body.name.toString();
+    let price = req.body.price.toString();
+    let description = req.body.description.toString();
+    let image = req.body.image.toString();
+    let select = req.body.category.toString();
+    let products_ = await Products.find({});
+
+    await Products.insertMany([             
+        {
+            "id": products_.length + 1,
+            "title": name,
+            "description": description,
+            "price": price,
+            "category": select,
+            "discountPercentage": ,
+            "rating": ,
+            "stock": ,
+            "brand": "",
+            "thumbnail": "",
+            "images": [
+                image,
+            ]
+        }]);
+    res.send('success: true');
+})
+
 app.get('/administrator', (req, res) => {
     res.render('template-administrator');
+})
+
+app.get('/shop', async (req, res) => {
+    products = await Products.find({});
+    res.render('template-shop', {products});
 })
 
 app.get('/delete-product/:id',async (req, res) => {
